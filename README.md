2afd437f5d0cffb98dfa0d4c610575f54d36ad24
Lucas Magalhães <lucasdarosa.ti@gmail.com>
2022-08-27 20:20:24 -0300

    Installing Tailwind CSS in React

--- README.md
+++ README.md
@@ -15,3 +15,20 @@ npm install react-icons --save
 ```
 
 ---
+
+[tailwindcss](https://tailwindcss.com/)
+```
+npm install -D tailwindcss@npm:@tailwindcss/postcss7-compat @tailwindcss/postcss7-compat postcss@^7 autoprefixer@^9 @tailwindcss/forms
+```
+
+**Craco**
+```
+npm install @craco/craco
+```
+
+**Setting Tailwind Config**
+```
+npx tailwindcss init
+```
+
+---
--- src/App.js
+++ src/App.js
@@ -2,8 +2,14 @@ import { BiArchive } from 'react-icons/bi';
 
 function App() {
   return (
-    <div>
-      <h1><BiArchive /> Your Appointments</h1>
+    <div className='App container mx-auto mt-3 font-thin'>
+      <h1 className='text-5xl'>
+        <BiArchive 
+          className='inline-block text-red-400 align-top' 
+        />
+        
+        Your Appointments
+      </h1>
     </div>
   );
 }
