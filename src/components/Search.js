11d5ed5cec4991c788552b8d655dceb9b5286213
Lucas Magalhães <lucasdarosa.ti@gmail.com>
2022-08-27 21:51:59 -0300

    Creating a sub-component Hooks

--- src/components/Search.js
+++ src/components/Search.js
@@ -1,4 +1,59 @@
-import { BiSearch, BiCaretDown } from 'react-icons/bi';
+import { BiSearch, BiCaretDown, BiCheck } from 'react-icons/bi';
+
+const DropDown = () => {
+    return (
+        <div 
+            className="
+                origin-top-right absolute right-0 mt-2 w-56
+                rounded-md shadow-lg bg-white ring-1
+                ring-black ring-opacity-5
+            "
+        >
+            <div className="py-1" role="menu" aria-orientation="vertical" aria-labelledby="options-menu">
+                <div
+                    className="px-4 py-2 text-sm text-gray-700 hover:bg-gray-100 hover:text-gray-900 flex justify-between cursor-pointer"
+                    role="menuitem"
+                >
+                    Pet Name <BiCheck />
+                </div>
+                
+                <div
+                    className="px-4 py-2 text-sm text-gray-700 hover:bg-gray-100 hover:text-gray-900 flex justify-between cursor-pointer"
+                    role="menuitem"
+                >
+                    Owner Name <BiCheck />
+                </div>
+                
+                <div
+                    className="px-4 py-2 text-sm text-gray-700 hover:bg-gray-100 hover:text-gray-900 flex justify-between cursor-pointer"
+                    role="menuitem"
+                >
+                    Date <BiCheck />
+                </div>
+                
+                <div
+                    className="
+                        px-4 py-2 text-sm text-gray-700 hover:bg-gray-100 hover:text-gray-900 
+                        flex justify-between cursor-pointer border-gray-1 border-t-2
+                    "
+                    role="menuitem"
+                >
+                    Asc <BiCheck />
+                </div>
+                
+                <div
+                    className="
+                        px-4 py-2 text-sm text-gray-700 hover:bg-gray-100 hover:text-gray-900 
+                        flex justify-between cursor-pointer
+                    "
+                    role="menuitem"
+                >
+                    Desc <BiCheck />
+                </div>
+            </div>
+      </div>
+    )
+}
 
 const Search = () => {
     return (
@@ -36,6 +91,8 @@ const Search = () => {
                         >
                             Sort By <BiCaretDown className="ml-2" />
                         </button>
+
+                        <DropDown />
                     </div>
                 </div>
             </div>
